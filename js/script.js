13962a8b82b3c698ed8a0d461523ddf540acc98d
hidri <agthidri2400@gmail.com>
2020-04-05 21:35:54 +0300

    This commit will add the javascript

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        autoplay:true,
+        smartSpedd:600,
+        loop:true,
+        autoplayHoverPause:true,
+        nav:true,
+        dots:false
+
+    });
+
+});
