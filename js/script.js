25bc8f6507ab41159cd7dc6ce0c23857dc86da65
hidri <agthidri2400@gmail.com>
2020-04-05 22:53:36 +0300

    this commit will add some javascript

--- js/script.js
+++ js/script.js
@@ -11,3 +11,41 @@ $(document).ready(function(){
     });
 
 });
+function validation(){
+    var name = document.getElementById("name").value;
+    var subject = document.getElementById("subject").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+    var email = document.getElementById("email").value;
+    var errorm = document.getElementById("errorm");
+    var text;
+
+   errorm.style.padding = "10px";
+
+   if (name.length < 2){
+       text = "please Enter Valid Name";
+       errorm.innerHTML = text;
+       return false ;
+   }
+  
+if (email.indexOf("@") == -1 || email.length < 5){
+  text = "please Enter Valid email";
+  errorm.innerHTML = text;
+  return false ;
+
+}
+alert("form sunmitted success")
+}
+
+$ (function(){
+    new Window().init();
+});
+$(window).on('load', function(){
+    $("#image-wine1").addClass("animated fadeInDown");
+    $("#image-wine2").addClass("animated fadeInDown");
+    $("#image-wine3").addClass("animated fadeInDown");
+
+    $(".wine-heading").addClass("animated fadeInLeft");
+    $(".contact-box").addClass("animated fadeInDown");
+    $("").addClass("animated fadeInDown infinite")
+})
